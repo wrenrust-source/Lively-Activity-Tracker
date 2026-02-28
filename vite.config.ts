70a1c40be03803d94c775bdab738d524b8e5471f
Tropical-Fire <56566807+Tropical-Fire@users.noreply.github.com>
2026-02-28 11:46:09 -0500

    push build files

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
@@ -16,7 +14,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-
+  build: {
+    target: 'es2020',
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+  },
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
